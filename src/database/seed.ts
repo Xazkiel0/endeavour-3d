27e7838ae2917388caf5f590852ec73b560fc8f8
Xazkiel0 <xazkiel@gmail.com>
2025-03-15 01:33:31 +0700

    fix: seed

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -5,7 +5,12 @@ import { drizzle } from "drizzle-orm/node-postgres";
 
 async function main() {
   const db = drizzle(process.env.DATABASE_URL!);
-  await seed(db, { users: schema.users });
+  await seed(db, { userss: schema.users }).refine((f) => ({
+    userss: {
+      columns: {},
+      count: 10,
+    },
+  }));
 }
 
 main();
