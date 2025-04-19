0e8c83bec44e72e8085a9b333f2caabfcc9b60d9
Xazkiel0 <xazkiel@gmail.com>
2025-04-20 00:32:43 +0700

    graphql issue

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,56 +1,56 @@
-import { buildSchema } from "drizzle-graphql";
-import { ApolloServer } from "@apollo/server";
-import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { db } from "../../../database/db";
-import {
-  GraphQLList,
-  GraphQLNonNull,
-  GraphQLObjectType,
-  GraphQLSchema,
-} from "graphql";
-import { users } from "@/database/schema/users";
+// import { buildSchema } from "drizzle-graphql";
+// import { ApolloServer } from "@apollo/server";
+// import { startServerAndCreateNextHandler } from "@as-integrations/next";
+// import { db } from "../../../database/db";
+// import {
+//   GraphQLList,
+//   GraphQLNonNull,
+//   GraphQLObjectType,
+//   GraphQLSchema,
+// } from "graphql";
+// import { users } from "@/database/schema/users";
 
-const { entities } = buildSchema(db);
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "Query",
-    fields: {
-      users: entities.queries.users,
-      user: entities.queries.usersSingle,
-      profiles: entities.queries.profiles,
-      profile: entities.queries.profilesSingle,
-    },
-  }),
-  mutation: new GraphQLObjectType({
-    name: "Mutation",
-    fields: {
-      createUser: {
-        type: new GraphQLList(new GraphQLNonNull(entities.types.UsersItem)),
-        args: {
-          where: {
-            type: entities.inputs.UsersFilters,
-          },
-          data: {
-            type: new GraphQLNonNull(entities.inputs.UsersInsertInput),
-          },
-        },
-        resolve: async (_, { data }) => {
-          const res = await db.insert(users).values(data).returning();
-          return res;
-        },
-      },
-    },
-  }),
-  types: [...Object.values(entities.types), ...Object.values(entities.inputs)],
-});
+// const { entities } = buildSchema(db);
+// const schema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: "Query",
+//     fields: {
+//       users: entities.queries.users,
+//       user: entities.queries.usersSingle,
+//       profiles: entities.queries.profiles,
+//       profile: entities.queries.profilesSingle,
+//     },
+//   }),
+//   mutation: new GraphQLObjectType({
+//     name: "Mutation",
+//     fields: {
+//       createUser: {
+//         type: new GraphQLList(new GraphQLNonNull(entities.types.UsersItem)),
+//         args: {
+//           where: {
+//             type: entities.inputs.UsersFilters,
+//           },
+//           data: {
+//             type: new GraphQLNonNull(entities.inputs.UsersInsertInput),
+//           },
+//         },
+//         resolve: async (_, { data }) => {
+//           const res = await db.insert(users).values(data).returning();
+//           return res;
+//         },
+//       },
+//     },
+//   }),
+//   types: [...Object.values(entities.types), ...Object.values(entities.inputs)],
+// });
 
-const server = new ApolloServer({
-  schema,
+// const server = new ApolloServer({
+//   schema,
 
-  // typeDefs,
-  // resolvers,
-});
+//   // typeDefs,
+//   // resolvers,
+// });
 
-const handler = startServerAndCreateNextHandler(server);
+// const handler = startServerAndCreateNextHandler(server);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
